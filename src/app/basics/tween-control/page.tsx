e889ceeac1f322ec411099595fbf5d1ab29d711d
niiko <nicolas.rocagel@gmail.com>
2023-08-09 11:19:26 +0200

    add tween-control

--- src/app/basics/tween-control/page.tsx
+++ src/app/basics/tween-control/page.tsx
@@ -3,13 +3,17 @@
 import { gsap } from "gsap";
 import { useEffect, useRef, useState } from "react";
 
+let tween: any;
+
 export default function Tween() {
   useEffect(() => {
-    gsap.fromTo(
+    tween = gsap.fromTo(
       "#freds img",
       {
         y: 600,
         x: 600,
+        opacity: 1,
+        rotate: 2,
       },
       {
         y: 200,
@@ -17,6 +21,7 @@ export default function Tween() {
         ease: "elastic.inOut",
         rotate: 20,
         duration: 3,
+        paused: true,
       }
     );
   }, []);
@@ -43,15 +48,40 @@ export default function Tween() {
   return (
     <div style={bground} id="freds" className="flex h-screen ">
       <img
-        className="w-32 h-32 monster1"
+        className="w-32 h-32 monster1 opacity-0"
         src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipart-library.com%2Fimages%2F6Tpo6nM6c.png&f=1&nofb=1&ipt=263746bba242785e0456e6707aed2ee70654d2fe9decfac3a72aac71bfc014d5&ipo=images"
         alt="monster"
       />
-      <img
-        className="w-32 h-32  monster2"
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpluspng.com%2Fimg-png%2Flil-monster-png--2388.png&f=1&nofb=1&ipt=9adffc060b8b184b90a6f607394b485aaa10201936aa3bb370189065d2f15016&ipo=images"
-        alt="monster"
-      />
+      <div className="flex w-screen justify-center mt-4">
+        <button
+          onClick={() => tween.play()}
+          className="btn btn-info m-2"
+          id="play"
+        >
+          play
+        </button>
+        <button
+          onClick={() => tween.pause()}
+          className="btn btn-info m-2"
+          id="pause"
+        >
+          pause
+        </button>
+        <button
+          onClick={() => tween.reverse()}
+          className="btn btn-info m-2"
+          id="reverse"
+        >
+          reverse
+        </button>
+        <button
+          onClick={() => tween.restart()}
+          className="btn btn-info m-2"
+          id="restart"
+        >
+          restart
+        </button>
+      </div>
     </div>
   );
 }
